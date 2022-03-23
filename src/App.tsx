23be83b0a49961a7387dcfbf3041178749de27f4
dujthep-odds <dujthep@odds.team>
2022-03-23 22:05:49 +0700

    Add lib web3 and ethers. Try to connect metamask.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { Web3Provider } from '@ethersproject/providers';
+import { Web3ReactProvider } from '@web3-react/core';
 import React from 'react';
 import './App.css';
 import ConnectWalletButton from './component/ConnectWalletButton';
 
+function getLibrary(provider: any): Web3Provider {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+  return library
+}
+
 function App() {
   return (
-    <div className="App">
-      <h1>Connect with MetaMask Demo</h1>
-      <ConnectWalletButton />
-    </div>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <div className="App">
+        <h1>Connect with MetaMask Demo</h1>
+        <ConnectWalletButton />
+      </div>
+    </Web3ReactProvider>
   );
 }
 
--- src/component/ConnectWalletButton.tsx
+++ src/component/ConnectWalletButton.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { useWeb3React } from "@web3-react/core";
+import React, { useCallback, useEffect, useState } from "react";
+import { injected } from "./connectors";
 
 const ConnectWalletButton = () => {
-  return <button>Connect Wallet</button>;
+  const { activate, active } = useWeb3React()
+  const [isActive, setIsActive] = useState(false)
+
+  const handleIsActive = useCallback(() => {
+    setIsActive(active)
+  }, [active])
+
+  useEffect(() => {
+    handleIsActive()
+  }, [handleIsActive])
+
+  const connect = async () => {
+    console.log('Connecting to MetaMask Wallet')
+    try {
+      await activate(injected)
+    } catch (error) {
+      console.log('Error on connecting: ', error)
+    }
+  }
+
+  return (
+    <div>
+      <label>{isActive}</label>
+      <button onClick={connect}>Connect Wallet</button>
+    </div>
+  );
 }
 
 export default ConnectWalletButton
